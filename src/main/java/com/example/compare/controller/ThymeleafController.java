e96828577fb13b65d047a243fdeca9bc05aff919
suger-no <1030699086@qq.com>
2022-03-27 09:25:44 +0800

    注释

--- src/main/java/com/example/compare/controller/ThymeleafController.java
+++ src/main/java/com/example/compare/controller/ThymeleafController.java
@@ -1,11 +1,20 @@
 package com.example.compare.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Api(value = "ThymeleafController",tags = "thymeleaf")
 public class ThymeleafController {
+    /**
+     * 跳转到收银台
+     * @param total_amount  商品金额
+     * @param out_trade_no  订单号
+     * @param model
+     * @return
+     */
     @RequestMapping("/index")
     public String index(String total_amount,String out_trade_no,Model model){
         model.addAttribute("total_amount",total_amount);
@@ -13,6 +22,13 @@ public class ThymeleafController {
         return "index";
     }
 
+    /**
+     *
+     * @param total_amount  商品金额
+     * @param out_trade_no  订单号
+     * @param model
+     * @return
+     */
     @RequestMapping("/succeed")
     public String succeed(String total_amount,String out_trade_no,Model model){
         model.addAttribute("total_amount",total_amount);
