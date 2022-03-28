7154457ed757a7f5e24f8ce8d215d1ec5308dd73
zcx <3412471320@qq.com>
2022-03-28 19:43:07 +0800

    对比记录查询结果中加入了两个对比文件的名称

--- src/main/java/com/example/compare/service/impl/CompareLogServiceImpl.java
+++ src/main/java/com/example/compare/service/impl/CompareLogServiceImpl.java
@@ -1,13 +1,17 @@
 package com.example.compare.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.compare.entity.CompareLog;
+import com.example.compare.mapper.AttachmentMapper;
 import com.example.compare.mapper.CompareLogMapper;
 import com.example.compare.service.CompareLogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -19,19 +23,20 @@ import java.util.List;
  */
 @Service
 public class CompareLogServiceImpl extends ServiceImpl<CompareLogMapper, CompareLog> implements CompareLogService {
-    @Autowired
-    CompareLogMapper compareLogMapper;
+    @Resource
+    private CompareLogMapper compareLogMapper;
+    @Resource
+    private AttachmentMapper attachmentMapper;
 
     @Override
     public Integer saveCompareLog(CompareLog compareLog) {
         compareLogMapper.insert(compareLog);
-        Integer id = compareLog.getId();
-        return id;
+        return compareLog.getId();
     }
 
     @Override
     public List<CompareLog> search(String keywords, String startTime, String endTime, int index, int maxPage) {
-        return compareLogMapper.search(keywords,startTime,endTime,index,maxPage);
+        return compareLogMapper.search(keywords, startTime, endTime, index, maxPage);
     }
 
     @Override
@@ -47,5 +52,18 @@ public class CompareLogServiceImpl extends ServiceImpl<CompareLogMapper, Compare
     @Override
     public List<CompareLog> selectList() {
         return compareLogMapper.selectList(null);
+    }
+
+    @Override
+    public IPage<Map<String, Object>> getCompareLogPageAndFilename(Long current, Long size) {
+        IPage<CompareLog> page = new Page<>(current, size);
+        IPage<Map<String, Object>> mapIPage = compareLogMapper.selectMapsPage(page, null);
+        for (Map<String, Object> record : mapIPage.getRecords()) {
+            String compare_file_1 = attachmentMapper.selectFileNameById((Integer) record.get("compare_file_1"));
+            String compare_file_2 = attachmentMapper.selectFileNameById((Integer) record.get("compare_file_2"));
+            record.put("compare_file_name_1", compare_file_1 == null ? "" : compare_file_1);
+            record.put("compare_file_name_2", compare_file_2 == null ? "" : compare_file_2);
+        }
+        return mapIPage;
     }
 }
