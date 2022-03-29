e75bd5ca9e278a16c47d3e528f3c8f966c311b69
zcx <3412471320@qq.com>
2022-03-29 09:25:23 +0800

    添加了对比分页接口

--- src/main/java/com/example/compare/service/CompareService.java
+++ src/main/java/com/example/compare/service/CompareService.java
@@ -1,5 +1,8 @@
 package com.example.compare.service;
 
-public interface CompareService {
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.compare.entity.Compare;
+
+public interface CompareService extends IService<Compare> {
     String getOrderIdById(Integer id);
 }
