0196ae10355495b0b85fd07d9d63f0e8103ac93f
“orange” <203152807@qq.com>
2022-03-29 09:32:44 +0800

    肖恒宇second提交 添加修改对比状态

--- src/main/java/com/example/compare/service/CompareService.java
+++ src/main/java/com/example/compare/service/CompareService.java
@@ -2,4 +2,11 @@ package com.example.compare.service;
 
 public interface CompareService {
     String getOrderIdById(Integer id);
+
+
+    /**
+     *  修改对比状态
+     */
+
+    public boolean updateCompareStatus(Integer id);
 }
