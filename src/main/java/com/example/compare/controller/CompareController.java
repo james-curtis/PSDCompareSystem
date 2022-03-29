87b1aa8f1ced8f07a2fecc70c8143f4a050d3499
suger-no <1030699086@qq.com>
2022-03-29 09:01:47 +0800

    增加redis，轮训接口

--- src/main/java/com/example/compare/controller/CompareController.java
+++ src/main/java/com/example/compare/controller/CompareController.java
@@ -1,9 +1,12 @@
 package com.example.compare.controller;
 
 import com.example.compare.common.utils.QRCodeUtil;
+import com.example.compare.common.utils.Result;
 import com.example.compare.service.CompareService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.imageio.ImageIO;
@@ -14,6 +17,9 @@ import java.io.IOException;
 public class CompareController {
     @Autowired
     CompareService service;
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
     /**
      * 获取跳转支付界面的二维码
      * @param id 对比记录id
@@ -29,5 +35,17 @@ public class CompareController {
         BufferedImage qr = QRCodeUtil.getBufferedImage(url, size);
         ImageIO.write(qr,"jpg",response.getOutputStream());
 //        return Result.success()
+    }
+
+    /**
+     * 轮训交易状态接口
+     * @param id compare记录id
+     * @return
+     */
+    @GetMapping("/getStatus")
+    @ApiOperation("刘锦堂===>轮训支付状态，id：compare记录表id")
+    public Result getStatus(Integer id){
+        String status =redisTemplate.opsForValue().get(id);
+        return Result.success(status);
     }
 }
