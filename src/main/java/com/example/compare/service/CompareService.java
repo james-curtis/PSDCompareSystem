4c968039d4ea14ae7f01933d1e5aadd439bde86f
郑前 <2324070916@qq.com>
2022-03-29 09:21:50 +0800

    first-commit

--- src/main/java/com/example/compare/service/CompareService.java
+++ src/main/java/com/example/compare/service/CompareService.java
@@ -1,5 +1,40 @@
 package com.example.compare.service;
 
+import com.example.compare.entity.Compare;
+
+import java.util.List;
+
 public interface CompareService {
     String getOrderIdById(Integer id);
+
+    /**
+     * 保存CompareLog信息
+     * @param compareLog
+     * @return
+     */
+    public Integer saveCompareLog(Compare compareLog);
+
+    /**
+     * 历史记录分页查询
+     * @return
+     */
+    List<Compare> search(String keywords, String startTime, String endTime, int index, int maxPage);
+
+    /**
+     * 多表删除
+     * @return
+     */
+    int allDelete(int orderId);
+
+    /**
+     * 根据id获取数据
+     * @return
+     */
+    Compare select(int id);
+
+    /**
+     * 获取所有数据
+     * @return
+     */
+    List<Compare> selectList();
 }
--- src/main/java/com/example/compare/service/impl/CompareServiceImpl.java
+++ src/main/java/com/example/compare/service/impl/CompareServiceImpl.java
@@ -1,14 +1,46 @@
 package com.example.compare.service.impl;
 
+import com.example.compare.entity.Compare;
 import com.example.compare.mapper.CompareMapper;
 import com.example.compare.service.CompareService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class CompareServiceImpl implements CompareService {
     @Autowired
     CompareMapper mapper;
     @Override
     public String getOrderIdById(Integer id) {
         return mapper.getOrderIdByid(id);
+    }
+
+    @Override
+    public Integer saveCompareLog(Compare compareLog) {
+        mapper.insert(compareLog);
+        Integer id = compareLog.getId();
+        return id;
+    }
+
+    @Override
+    public List<Compare> search(String keywords, String startTime, String endTime, int index, int maxPage) {
+        return mapper.search(keywords,startTime,endTime,index,maxPage);
+    }
+
+    @Override
+    public int allDelete(int orderId) {
+        return mapper.allDelete(orderId);
+    }
+
+    @Override
+    public Compare select(int id) {
+        return mapper.selectById(id);
+    }
+
+    @Override
+    public List<Compare> selectList() {
+        return mapper.selectList(null);
     }
 }
