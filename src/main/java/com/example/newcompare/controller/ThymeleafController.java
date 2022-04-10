ac07eb9113028fcc9e7f741c63619b22346d31de
suger-no <1030699086@qq.com>
2022-04-11 07:29:01 +0800

    支付

--- src/main/java/com/example/newcompare/controller/ThymeleafController.java
+++ src/main/java/com/example/newcompare/controller/ThymeleafController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
-@Controller
+@Controller("/thymeleaf")
 @Api(value = "ThymeleafController",tags = "thymeleaf")
 public class ThymeleafController {
     @Autowired
@@ -35,14 +35,14 @@ public class ThymeleafController {
      * @param model
      * @return
      */
-//    @ApiOperation(value = "跳转收银台界面， outTradeId: 订单编号")
-//    @RequestMapping("/index")
-//    public String index(String outTradeId,Model model){
-//        OrderLog orderLog = service.getOrderLog(outTradeId);
-//        model.addAttribute("total_amount",orderLog.getFee());
-//        model.addAttribute("out_trade_no",orderLog.getOutTradeId());
-//        return "index";
-//    }
+    @ApiOperation(value = "跳转收银台界面， outTradeId: 订单编号")
+    @RequestMapping("/index")
+    public String index(String outTradeId,Model model){
+        OrderLog orderLog = service.getById(outTradeId);
+        model.addAttribute("total_amount",orderLog.getFee());
+        model.addAttribute("out_trade_no",orderLog.getOutTradeId());
+        return "index";
+    }
 
     /**
      *
@@ -51,30 +51,30 @@ public class ThymeleafController {
      * @return
      * @throws AlipayApiException
      */
-//    @RequestMapping("/succeed")
-//    public String succeed(Model model, HttpServletRequest request) throws AlipayApiException {
-//
-//        String out_trade_no = request.getParameter("out_trade_no");
-//        String total_amount = request.getParameter("total_amount");
-//        String msg = null;
-//        //进行同步验签
-//        Map<String, String> paramsMap = ChangeToMapUtil.convertRequestParamsToMap(request);
-//        boolean signVerified = AlipaySignature.rsaCheckV1(paramsMap, alipayUtil.getPublicKey(), paramsMap.get("charset"), paramsMap.get("sign_type")); //调用SDK验证签名
-//        if (signVerified) {//验签成功
-//
-//            if (!service.checkOrderAndUpdateDatabase(out_trade_no)) {
-//                msg = "支付失败";
-//            } else {
-//                msg = "支付成功";
-//            }
-//        } else {
-//            msg = "支付失败";
-//            //这里给出错误页面。如果验签失败绝对是非法入侵
-//        }
-//        model.addAttribute("total_amount", total_amount);
-//        model.addAttribute("out_trade_no", out_trade_no);
-//        model.addAttribute("msg", msg);
-//        return "succeed";
-//    }
+    @RequestMapping("/payFinished")
+    public String succeed(Model model, HttpServletRequest request) throws AlipayApiException {
+
+        String out_trade_no = request.getParameter("out_trade_no");
+        String total_amount = request.getParameter("total_amount");
+        String msg = null;
+        //进行同步验签
+        Map<String, String> paramsMap = ChangeToMapUtil.convertRequestParamsToMap(request);
+        boolean signVerified = AlipaySignature.rsaCheckV1(paramsMap, alipayUtil.getPublicKey(), paramsMap.get("charset"), paramsMap.get("sign_type")); //调用SDK验证签名
+        if (signVerified) {//验签成功
+
+            if (!service.checkOrderAndUpdateDatabase(out_trade_no)) {
+                msg = "支付失败";
+            } else {
+                msg = "支付成功";
+            }
+        } else {
+            msg = "支付失败";
+            //这里给出错误页面。如果验签失败绝对是非法入侵
+        }
+        model.addAttribute("total_amount", total_amount);
+        model.addAttribute("out_trade_no", out_trade_no);
+        model.addAttribute("msg", msg);
+        return "succeed";
+    }
 
 }
--- src/main/java/com/example/newcompare/service/impl/OrderLogServiceImpl.java
+++ src/main/java/com/example/newcompare/service/impl/OrderLogServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.newcompare.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.newcompare.common.utils.AlipayUtil;
 import com.example.newcompare.entity.OrderLog;
 import com.example.newcompare.mapper.OrderLogMapper;
 import com.example.newcompare.service.OrderLogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +19,40 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OrderLogServiceImpl extends ServiceImpl<OrderLogMapper, OrderLog> implements OrderLogService {
+    @Autowired
+    AlipayUtil alipayUtil;
 
+    @Autowired
+    OrderLogMapper mapper;
+
+    @Override
+    public OrderLog getOrderLog(String outTradeId) {
+        OrderLog orderLog = mapper.selectOne(new QueryWrapper<OrderLog>().eq("out_trade_id",outTradeId));
+        return orderLog;
+    }
+
+
+    /**
+     * 查询订单支付状态并且更新数据库
+     *
+     * @param outTradeNo
+     * @return
+     */
+    @Override
+    public boolean checkOrderAndUpdateDatabase(String outTradeNo) {
+        //判断数据库里面的支付状态
+        OrderLog orderLog = this.getOrderLog(outTradeNo);
+        if (orderLog.getStatus().equals("unpaid")) {
+            //查单
+            String status = alipayUtil.queryTradeStatus(outTradeNo);
+            if (status.equals("TRADE_SUCCESS")) {
+                //支付宝那边支付成功
+                orderLog.setStatus("complete");
+                return mapper.updateById(orderLog) > 0;
+            }
+        } else if (orderLog.getStatus().equals("complete")) {
+            return true;
+        }
+        return false;
+    }
 }
