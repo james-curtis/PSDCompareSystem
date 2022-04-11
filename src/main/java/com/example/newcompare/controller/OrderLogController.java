9fa77f56fa3bc7178aaaa9edad799ad020f5980a
suger-no <1030699086@qq.com>
2022-04-11 13:06:34 +0800

    修改实体信息

--- src/main/java/com/example/newcompare/controller/OrderLogController.java
+++ src/main/java/com/example/newcompare/controller/OrderLogController.java
@@ -1,9 +1,24 @@
 package com.example.newcompare.controller;
 
 
+import com.example.newcompare.common.utils.QRCodeUtil;
+import com.example.newcompare.common.utils.Result;
+import com.example.newcompare.entity.OrderLog;
+import com.example.newcompare.service.OrderLogService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +31,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/order-log")
 public class OrderLogController {
-
+    @Resource
+    OrderLogService service;
+    /**
+     * 获取跳转支付界面的二维码
+     * @param size 二维码大小
+     * @param response
+     * @throws IOException
+     */
+    @ApiOperation("刘锦堂===>获取跳转支付界面的二维码，id：compare表id，size： 二维码大小，默认值250")
+    @GetMapping("/order-log")
+    public void getQRCode(Integer id, Integer size, HttpServletResponse response) throws IOException {
+        String url = "http://114.55.0.204:8081/thymeleaf/index?outTradeId="+service.getById(id).getOutTradeId();
+        BufferedImage qr = QRCodeUtil.getBufferedImage(url, size);
+        ImageIO.write(qr,"jpg",response.getOutputStream());
+//        return Result.success()
+    }
 }
--- src/test/java/com/example/newcompare/NewCompareApplicationTests.java
+++ src/test/java/com/example/newcompare/NewCompareApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.newcompare;
 
+import com.baomidou.mybatisplus.core.conditions.interfaces.Compare;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NewCompareApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
+//
+//    @Autowired
+//    CompareMapper mapper;
+//    @Test
+//    void contextLoads() {
+////        final List<Compare> compares = mapper.selectList(null);
+////        for (Compare compare : compares) {
+////            System.out.println(compare);
+////        }
+//        mapper.delete(new UpdateWrapper<Compare>().eq("id","209"));
+//
+//    }
 
 }
