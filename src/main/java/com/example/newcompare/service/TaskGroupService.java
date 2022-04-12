3029c728ff16bdbdfdcae4e00371cfa43d442292
suger-no <1030699086@qq.com>
2022-04-12 14:45:21 +0800

    create workGroup

--- src/main/java/com/example/newcompare/service/TaskGroupService.java
+++ src/main/java/com/example/newcompare/service/TaskGroupService.java
@@ -3,6 +3,9 @@ package com.example.newcompare.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.newcompare.entity.TaskGroup;
 import com.example.newcompare.entity.WorkCode;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface TaskGroupService extends IService<TaskGroup> {
+    Integer create(TaskGroup taskGroup);
 }
