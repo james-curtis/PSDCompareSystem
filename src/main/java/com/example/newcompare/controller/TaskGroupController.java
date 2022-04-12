cb7d6081277c175958646d78bb529097b4e67709
suger-no <1030699086@qq.com>
2022-04-12 14:45:21 +0800

    create workGroup

--- src/main/java/com/example/newcompare/controller/TaskGroupController.java
+++ src/main/java/com/example/newcompare/controller/TaskGroupController.java
@@ -1,9 +1,31 @@
 package com.example.newcompare.controller;
 
+import com.example.newcompare.common.utils.Result;
+import com.example.newcompare.entity.TaskGroup;
+import com.example.newcompare.service.TaskGroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/taskGroup")
 public class TaskGroupController {
+    @Autowired
+    TaskGroupService service;
+    @PostMapping("/create")
+    public Result create(String name){
+        TaskGroup taskGroup = new TaskGroup();
+        taskGroup.setName(name);
+        taskGroup.setCreateTime(LocalDateTime.now());
+        taskGroup.setWorkCode(UUID.randomUUID().toString());
+        Integer i = service.create(taskGroup);
+        if(i==0){
+            return Result.fail("插入失败");
+        }
+        return Result.success("插入成功");
+    }
 }
--- src/main/java/com/example/newcompare/service/impl/TaskGroupServiceImpl.java
+++ src/main/java/com/example/newcompare/service/impl/TaskGroupServiceImpl.java
@@ -6,5 +6,13 @@ import com.example.newcompare.entity.WorkCode;
 import com.example.newcompare.mapper.TaskGroupMapper;
 import com.example.newcompare.service.TaskGroupService;
 
+import javax.annotation.Resource;
+
 public class TaskGroupServiceImpl extends ServiceImpl<TaskGroupMapper, TaskGroup> implements TaskGroupService {
+    @Resource
+    TaskGroupMapper mapper;
+    @Override
+    public Integer create(TaskGroup taskGroup) {
+        return mapper.insert(taskGroup);
+    }
 }
