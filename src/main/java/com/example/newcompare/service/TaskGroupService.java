36626382a8a4128ca22f85b8f3ec55c0b61942d4
suger-no <1030699086@qq.com>
2022-04-12 14:38:08 +0800

    修改名字问题

--- src/main/java/com/example/newcompare/service/TaskGroupService.java
+++ src/main/java/com/example/newcompare/service/TaskGroupService.java
@@ -1,7 +1,8 @@
 package com.example.newcompare.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.newcompare.entity.TaskGroup;
 import com.example.newcompare.entity.WorkCode;
 
-public interface WorkCodeService extends IService<WorkCode> {
+public interface TaskGroupService extends IService<TaskGroup> {
 }
---
+++ src/main/java/com/example/newcompare/service/impl/TaskGroupServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.newcompare.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.newcompare.entity.TaskGroup;
+import com.example.newcompare.entity.WorkCode;
+import com.example.newcompare.mapper.TaskGroupMapper;
+import com.example.newcompare.service.TaskGroupService;
+
+public class TaskGroupServiceImpl extends ServiceImpl<TaskGroupMapper, TaskGroup> implements TaskGroupService {
+}
