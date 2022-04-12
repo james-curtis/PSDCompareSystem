00b1cf90076a0cf5eab532f3fb6383ef20b0eb8b
suger-no <1030699086@qq.com>
2022-04-12 08:17:54 +0800

    mapper

---
+++ src/main/java/com/example/newcompare/service/impl/WorkCodeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.newcompare.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.newcompare.entity.WorkCode;
+import com.example.newcompare.mapper.WorkCodeMapper;
+import com.example.newcompare.service.WorkCodeService;
+
+public class WorkCodeServiceImpl extends ServiceImpl<WorkCodeMapper, WorkCode> implements WorkCodeService {
+}
