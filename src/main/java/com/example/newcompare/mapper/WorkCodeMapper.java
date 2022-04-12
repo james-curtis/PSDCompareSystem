65fef47b7297bb688b593bc910b5674f1c624800
suger-no <1030699086@qq.com>
2022-04-12 08:17:54 +0800

    mapper

---
+++ src/main/java/com/example/newcompare/mapper/WorkCodeMapper.java
@@ -0,0 +1,7 @@
+package com.example.newcompare.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.newcompare.entity.WorkCode;
+
+public interface WorkCodeMapper extends BaseMapper<WorkCode> {
+}
---
+++ src/main/java/com/example/newcompare/service/WorkCodeService.java
@@ -0,0 +1,7 @@
+package com.example.newcompare.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.newcompare.entity.WorkCode;
+
+public interface WorkCodeService extends IService<WorkCode> {
+}
