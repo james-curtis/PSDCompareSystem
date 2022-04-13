dad03f7083f918287f1c3368e572533d0c7ee1ad
LC <2763866934@qq.com>
2022-04-13 19:00:40 +0800

    lc first commit fileupload and startcompare

--- src/main/java/com/example/newcompare/service/impl/FileServiceImpl.java
+++ src/main/java/com/example/newcompare/service/impl/FileServiceImpl.java
@@ -4,7 +4,10 @@ import com.example.newcompare.entity.File;
 import com.example.newcompare.mapper.FileMapper;
 import com.example.newcompare.service.FileService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +19,24 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FileServiceImpl extends ServiceImpl<FileMapper, File> implements FileService {
+
+    @Autowired
+    private FileMapper fileMapper;
+
+    
+    @Override
+    public Integer insertFile(File file) {
+        fileMapper.insert(file);
+        Integer id = file.getId();
+        return id;
+    }
+
+    @Override
+    public String seleceFileById(Integer fileId) {
+        File file = fileMapper.selectById(fileId);
+        String filecode = file.getFilecode();
+        return filecode;
+    }
+
 
 }
