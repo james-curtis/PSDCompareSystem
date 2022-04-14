3b098bf96696b7e3f8d3ffbfff9ff204528cbfc2
“orange” <203152807@qq.com>
2022-04-14 13:26:14 +0800

    多文件下载和回调

--- src/main/java/com/example/newcompare/NewCompareApplication.java
+++ src/main/java/com/example/newcompare/NewCompareApplication.java
@@ -1,11 +1,12 @@
 package com.example.newcompare;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NewCompareApplication {
 
+public class NewCompareApplication {
     public static void main(String[] args) {
         SpringApplication.run(NewCompareApplication.class, args);
     }
--- src/main/java/com/example/newcompare/controller/OrderLogController.java
+++ src/main/java/com/example/newcompare/controller/OrderLogController.java
@@ -1,7 +1,11 @@
 package com.example.newcompare.controller;
 
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.newcompare.common.utils.AlipayUtil;
+import com.example.newcompare.common.utils.ChangeToMapUtil;
 import com.example.newcompare.common.utils.QRCodeUtil;
 import com.example.newcompare.common.utils.Result;
 import com.example.newcompare.entity.OrderLog;
@@ -9,6 +13,7 @@ import com.example.newcompare.service.OrderLogService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -37,6 +43,11 @@ import java.util.concurrent.TimeUnit;
 @Controller
 @RequestMapping("/order-log")
 public class OrderLogController {
+
+    @Autowired
+    @Qualifier("alipayUtil")
+    AlipayUtil alipayUtil;
+
     @Resource
     OrderLogService service;
 
@@ -99,4 +110,59 @@ public class OrderLogController {
         redisTemplate.opsForValue().set(id,"未支付",15, TimeUnit.MINUTES);
         return "pay";
     }
+
+
+
+    /**
+     * 获取支付宝返回的订单信息
+     * @param request
+     * @param response
+     *
+     */
+
+    @ApiOperation("肖恒宇===>获取支付宝返回的订单信息")
+    @PostMapping("/notify")
+    public void  getAiLiPay(HttpServletRequest request, HttpServletResponse response) throws AlipayApiException, IOException {
+
+        Map<String,String> paramsMap= ChangeToMapUtil.convertRequestParamsToMap(request);
+        boolean signVerified = AlipaySignature.rsaCheckV1(paramsMap, alipayUtil.getPublicKey() ,paramsMap.get("charset"), paramsMap.get("sign_type")); //调用SDK验证签名
+        if(signVerified){
+            // 验签成功后，按照支付结果异步通知中的描述，对支付结果中的业务内容进行二次校验，校验成功后在response中返回success并继续商户自身业务处理，校验失败返回failure
+            OrderLog out_trade_no = service.getDiffInformation(paramsMap.get("out_trade_no"));
+
+            if(alipayUtil.getAppid().equals(paramsMap.get("app_id"))){
+
+            }
+            else if(paramsMap.get("out_trade_no").equals(out_trade_no.getOutTradeId())){
+
+            }
+            else if(paramsMap.get("total_amount").equals( out_trade_no.getFee().toString())){
+
+            }else{
+                response.getWriter().print("fail");
+                return;
+            }
+
+            if(paramsMap.get("trade_status").equals("TRADE_SUCCESS") || paramsMap.get("trade_status").equals("TRADE_FINISHED")){
+
+
+                if( service.updateStatus(out_trade_no)){
+                    response.getWriter().print("success");
+                }else {
+                    response.getWriter().print("fail");
+                }
+
+            }
+
+        }else{
+            // 验签失败则记录异常日志，并在response中返回failure.
+            response.getWriter().print("fail");
+        }
+    }
+
+
+
+
+
+
 }
