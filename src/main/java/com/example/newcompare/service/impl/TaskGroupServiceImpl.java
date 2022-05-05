44f71774dd6bf1a49edc878d6ca2b216ed9454c8
suger-no <1030699086@qq.com>
2022-05-05 18:00:37 +0800

    fenye

--- src/main/java/com/example/newcompare/service/impl/TaskGroupServiceImpl.java
+++ src/main/java/com/example/newcompare/service/impl/TaskGroupServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.newcompare.entity.TaskGroup;
 import com.example.newcompare.mapper.OrderLogMapper;
 import com.example.newcompare.mapper.TaskGroupMapper;
 import com.example.newcompare.service.TaskGroupService;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -38,8 +39,9 @@ public class TaskGroupServiceImpl extends ServiceImpl<TaskGroupMapper, TaskGroup
     }
 
     @Override
-    public Page<TaskGroup> getGroups(Page<TaskGroup> Page, String keyWords, String startTime, String endTime, String defaultSort) {
-        return mapper.getGroups(Page, keyWords, startTime, endTime, defaultSort);
+    public PageInfo<TaskGroup> getGroups(int maxpage, String keyWords, String startTime, String endTime, String defaultSort) {
+        PageInfo pageInfo = new PageInfo(mapper.getGroups( maxpage,keyWords, startTime, endTime, defaultSort), maxpage);
+        return pageInfo;
     }
 
     @Override
